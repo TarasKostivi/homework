7731cd610156ee1ead887ec37520ddbf62f5480a
TarasKostivi <taraskostsv@gmail.com>
2020-12-07 09:24:56 +0200

    Hw 1, 2, 3,

---
+++ src/hw3/Main1.java
@@ -0,0 +1,18 @@
+package hw3;
+
+import java.util.Scanner;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Put money in the Bank ::");
+        int m = scanner.nextInt();
+        double n = 0.07;
+        int r = 7;
+        for (int i = 0; i < r; i++) {
+            System.out.println(i);
+            n += n * m / 100;
+        }
+        System.out.print(n);
+    }
+}
