7a448d429557561f76eaf176a51ea53acb85ba26
TarasKostivi <taraskostsv@gmail.com>
2020-12-15 16:24:12 +0200

    homeWork6

---
+++ src/homeWork6/Flowers/Roses.java
@@ -0,0 +1,22 @@
+package homeWork6.Flowers;
+
+public class Roses extends Flowers{
+    private String name;
+    public Roses(String color, double price, String name) {
+        super(color, price);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean ollFlowers() {
+        return false;
+    }
+}
