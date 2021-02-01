79af6baa243643f5ccbc7ab01884ff52e280466a
TarasKostivi <taraskostsv@gmail.com>
2021-02-01 10:23:26 +0200

    hw 12/13

---
+++ src/homeWork12/hW3/Person.java
@@ -0,0 +1,54 @@
+package homeWork12.hW3;
+
+public class Person implements Comparable<Person>{
+
+    private String name;
+    private Integer age;
+    private String HeirColor;
+
+    public Person(String name, Integer age, String heirColor) {
+        this.name = name;
+        this.age = age;
+        HeirColor = heirColor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getHeirColor() {
+        return HeirColor;
+    }
+
+    public void setHeirColor(String heirColor) {
+        HeirColor = heirColor;
+    }
+
+    @Override
+    public String toString() {
+        return this.getName() + " " + this.getAge() + " "  + this.getHeirColor();
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        if (this.getHeirColor().compareTo(o.getHeirColor()) > 0){
+            return 1;
+        }else if (this.getHeirColor().compareTo(o.getHeirColor()) < 0){
+            return -1;
+        }else {
+            return  0;
+        }
+    }
+}
